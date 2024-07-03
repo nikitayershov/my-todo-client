94fd5b14d2a7c65a9a9cd55489cf96da63152efa
Nikita Yershov <nikita.yershov@gmail.com>
2024-07-03 15:23:50 +0300

    first commit

---
+++ src/api/readTodosRequest.js
@@ -0,0 +1,22 @@
+import { API_URL } from "./config";
+
+export default async (token) => {
+  try {
+    const response = await fetch(`${API_URL}/todos`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch todos');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
